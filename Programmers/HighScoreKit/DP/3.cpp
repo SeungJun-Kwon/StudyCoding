14a49806e3e14f86a567c1350906e0361696ab02
SeungJun-Kwon <tyrnsorl123@naver.com>
2022-02-13 22:54:00 +0900

    HighScoreKit_DP_3

--- Programmers/HighScoreKit/DP/3.cpp
+++ Programmers/HighScoreKit/DP/3.cpp
@@ -1,31 +1,34 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 1, cnt = 1, sum = 0, idx = 0;
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
     
-    queue<pair<int, int>> q;
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
     
-    q.push({truck_weights[idx++], cnt++});
-    sum += q.front().first;
-    while(!q.empty()) {
-        if(cnt == q.front().second + bridge_length) {
-            sum -= q.front().first;
-            printf("pop : %d %d %d\n", q.front().first, q.front().second, cnt);
-            q.pop();
+    for(int i = 0; i < puddles.size(); i++)
+        dp[puddles[i][1]][puddles[i][0]] = -1;
+    
+    dp[1][1] = 1;
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(dp[i][j] == -1 || (i == 1 && j == 1))
+                continue;
+            int a = 0, b = 0;
+            
+            if(dp[i - 1][j] != -1)
+                a = dp[i - 1][j];
+            if(dp[i][j - 1] != -1)
+                b = dp[i][j - 1];
+            
+            dp[i][j] = (a + b) % 1000000007;
         }
-        if(sum + truck_weights[idx] <= weight && idx < truck_weights.size()) {
-            sum += truck_weights[idx];
-            printf("push : %d %d %d\n", sum, truck_weights[idx], cnt);
-            q.push({truck_weights[idx++], cnt});
-        }
-        cnt ++;
     }
     
-    answer = cnt - 1;
+    answer = dp[n][m];
     
     return answer;
 }
