c62e3630f155e6db2f286a33e548f2fd4ab7e171
SeungJun-Kwon <tyrnsorl123@naver.com>
2022-02-25 14:10:28 +0900

    HighScoreKit_BinarySearch_1

--- Programmers/HighScoreKit/BinarySearch/1.cpp
+++ Programmers/HighScoreKit/BinarySearch/1.cpp
@@ -5,11 +5,27 @@
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-    long long answer = 28;
+    long long answer = 0;
     
     sort(times.begin(), times.end());
     
+    long long left = 1, right = times[times.size() - 1] * n, mid, cnt = 0;
     
+    while(left <= right) {
+        mid = (left + right) / 2;
+        
+        for(auto i : times)
+            cnt += mid / i;
+        
+        if(cnt >= n) {
+            answer = mid;
+            right = mid - 1;
+        }
+        else
+            left = mid + 1;
+        
+        cnt = 0;
+    }
     
     return answer;
 }
