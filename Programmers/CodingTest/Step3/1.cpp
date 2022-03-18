ef1af684d981256a52e502c8993173dd589c46d1
SeungJun-Kwon <tyrnsorl123@naver.com>
2022-03-18 23:48:30 +0900

    Programmers_CodingTest_Step3_1

--- Programmers/CodingTest/Step3/1.cpp
+++ Programmers/CodingTest/Step3/1.cpp
@@ -1,51 +1,27 @@
+#include <iostream>
 #include <string>
-#include <vector>
-#include <cmath>
-
+#include <stack>
 using namespace std;
 
-int solution(string s) {
-    int answer = s.size(), cnt = 0, res = 0;
-    int i = 0, j = 0;
+int solution(string s)
+{
+    int answer = 0;
     
-    for(i = 1; i <= s.size(); i++) {
-        string cmp = s.substr(0, i);
-        cnt = i;
-        res = 0;
-        for(j = i; j < s.size(); j += i) {
-            if(cmp != s.substr(j, i)) {
-                if(cnt <= i)
-                    res += i;
-                else if((cnt / i) < 10)
-                    res += i + 1;
-                else if((cnt / i) < 100)
-                    res += i + 2;
-                else if((cnt / i) < 1000)
-                    res += i + 3;
-                if(j + i >= s.size())
-                    break;
-                cnt = i;
-                cmp = s.substr(j, i);
-            }
-            else
-                cnt += i;
-        }
-        if(j < s.size()) {
-            res += s.size() - j;
-        }
-        else {
-            if(cnt <= i)
-                res += i;
-            else if((cnt / i) < 10)
-                res += i + 1;
-            else if((cnt / i) < 100)
-                res += i + 2;
-            else if((cnt / i) < 1000)
-                res += i + 3;
-        }
-        
-        answer = min(answer, res);
+    stack<char> st;
+    
+    st.push(s[0]);
+    
+    for(int i = 1; i < s.size(); i++) {
+        if(st.empty())
+            st.push(s[i]);
+        else if(s[i] != st.top())
+            st.push(s[i]);
+        else
+            st.pop();
     }
     
+    if(st.empty())
+        answer = 1;
+
     return answer;
 }
