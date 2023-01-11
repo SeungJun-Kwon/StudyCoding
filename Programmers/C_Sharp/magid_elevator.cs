5283575ed96603786617847299a1f07a1990def2
SeungJun-Kwon <tyrnsorl123@naver.com>
2023-01-12 00:41:00 +0900

    20230112
    Magic Elevator
    #Programmers

---
+++ Programmers/C_Sharp/magid_elevator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int storey)
+    {
+        int answer = 0;
+        int cal_storey = storey;
+
+        while (cal_storey > 0)
+        {
+            int r = cal_storey % 10;
+
+            cal_storey /= 10;
+
+            if (r < 5)
+            {
+                answer += r;
+            }
+            else if (r > 5)
+            {
+                answer += (10 - r);
+                cal_storey += 1;
+            }
+            else
+            {
+                answer += r;
+                if (cal_storey % 10 >= 5)
+                    cal_storey += 1;
+            }
+        }
+
+        return answer;
+    }
+}
