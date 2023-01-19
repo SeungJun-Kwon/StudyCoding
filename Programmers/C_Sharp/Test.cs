2f02d4709215825837318e77c27a17047bd24deb
SeungJun-Kwon <tyrnsorl123@naver.com>
2023-01-19 23:37:22 +0900

    20230119

---
+++ Programmers/C_Sharp/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+public class Class1
+{
+	string s1, s2;
+	Queue<int, int> q = new Queue<int, int>;
+
+	void abc()
+	{
+		q.Enqueue(0, 1);
+		q.Enqueue();
+	}
+}
---
+++ Programmers/Cpp/test.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
+    vector<int> answer;
+    string s;
+
+    for(int i = 0; i < privacies.size())
+
+    return answer;
+}
